b15eb7442a3ebb6483765687b2e63126dbbf63ee
Lily Li <23038037+mouyun-lily-li@users.noreply.github.com>
2018-12-09 04:34:49 -0600

    added code for svm model

--- facial_detection_training.py
+++ facial_detection_training.py
@@ -4,12 +4,20 @@ from skimage.transform import resize
 # from sklearn.linear_model import LogisticRegression
 from facial_detection_util import load_image, canny_nmax
 from facial_detection import compute_cell_histogram
+from sklearn import svm
 
 # orientation bins
 orient_bins = [0, np.pi / 9, (2 * np.pi) / 9, np.pi / 3, (4 * np.pi) / 9,
     (5 * np.pi) / 9, (2 * np.pi) / 3, (7 * np.pi) / 9, (8 * np.pi) / 9, np.pi]
 
 
+'''
+    Create training set data
+
+    Returns:
+        X_train (np array) - features of training set
+        y_train (np array) - labels of training set
+'''
 def create_training_x_y(cell_len = 8): # test diff cell sizes, since 8x8 was for human detection
     X_train = []
     y_train = []
@@ -35,7 +43,28 @@ def create_training_x_y(cell_len = 8): # test diff cell sizes, since 8x8 was for
                     mag_section = mag[j:j + cell_len, j:j + cell_len]
                     theta_section = theta[j:j + cell_len, j:j + cell_len]
                     hist = compute_cell_histogram(mag_section, theta_section, orient_bins)
+    # code to add, block norm
+
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
+
+    return X_train, y_train
+
+# FOR TESTING, NEED TO THINK ABOUT SCALE OF IMAGE
 
 
+'''
+    Train the model
 
-# FOR TESTING, NEED TO THINK ABOUT SCALE
+    Returns:
+        model (sklearn model) - SVM model that predicts the probability
+            an image is a face
+'''
+def train_model():
+    X_train, y_train = create_training_x_y()
+    #create and train an svm classifier
+    model = svm.SVC(kernel='linear')
+    model.fit(X_train, y_train)
+    print("Training accuracy:", model.score(X_train, y_train))
+    return model
+
